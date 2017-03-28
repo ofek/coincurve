6570018282ae36e90e34c755416d27182cc57013
ofek <ofekmeister@gmail.com>
2017-03-28 13:43:23 -0400

    derp

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,7 +12,7 @@ python setup.py sdist
 
 # On linux we want to build `manylinux1` wheels. See:
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
-	docker run --rm -v -e "BUILD_LINUX_WHEELS=1" $(pwd):/io ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
+	docker run --rm -e BUILD_LINUX_WHEELS -v $(pwd):/io ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
 else
 	# Only build wheels for the non experimental bundled version
 	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then

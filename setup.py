ac59adfe1ba9a75f8793700c55eea7601818e2af
ofek <ofekmeister@gmail.com>
2017-03-28 02:08:50 -0400

    make pypi happy

--- setup.py
+++ setup.py
@@ -250,7 +250,7 @@ class develop(_develop):
 
 setup(
     name="coincurve",
-    version="1.0.0",
+    version="1.0.1",
 
     description='Cross-platform Python CFFI bindings for libsecp256k1',
     long_description=open('README.md', 'r').read(),

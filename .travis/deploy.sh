ed599cb41dba31458e6cf330a2a3ea31f7740ed6
ofek <ofekmeister@gmail.com>
2017-03-28 01:30:56 -0400

    fix docker

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,7 +12,7 @@ python setup.py sdist
 
 # On linux we want to build `manylinux1` wheels. See:
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
-	docker run --rm -v $(pwd):/io ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
+	docker run --rm -v $(pwd):/io $chmod +x /io/.travis/build-linux-wheels.sh ${WHEELBUILDER_IMAGE} /io/.travis/build-linux-wheels.sh
 else
 	# Only build wheels for the non experimental bundled version
 	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then

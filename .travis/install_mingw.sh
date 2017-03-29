ffbeccfed5fc9624fe443eba08d4880cc094cc39
ofek <ofekmeister@gmail.com>
2017-03-29 01:39:41 -0400

    save clean clone for later

--- .travis/install_mingw.sh
+++ .travis/install_mingw.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
+    mkdir .hidden
+    cp * .hidden -R
+    mv .hidden ../clean
     sudo apt-get install -y mingw-w64
     sudo apt-get -f install
 fi

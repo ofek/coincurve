cb906f9e92781a3993b2550844b0a1a7c87d49b1
ofek <ofekmeister@gmail.com>
2017-03-29 22:21:45 -0400

    bump version

--- setup.py
+++ setup.py
@@ -270,7 +270,7 @@ else:
 
 setup(
     name='coincurve',
-    version='2.0.0',
+    version='2.1.0',
 
     description='Cross-platform Python CFFI bindings for libsecp256k1',
     long_description=open('README.md', 'r').read(),

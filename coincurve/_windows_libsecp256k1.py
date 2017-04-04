aadddc66fe40922a203109ee3cace62a899a99b3
ofek <ofekmeister@gmail.com>
2017-04-03 23:58:55 -0400

    fix ffi

--- coincurve/_windows_libsecp256k1.py
+++ coincurve/_windows_libsecp256k1.py
@@ -234,14 +234,14 @@ int ec_privkey_export_der(
     size_t *privkeylen,
     const unsigned char *seckey,
     int compressed
-) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
+);
 
 int ec_privkey_import_der(
     const secp256k1_context* ctx,
     unsigned char *seckey,
     const unsigned char *privkey,
     size_t privkeylen
-) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3);
+);
 """
 
 ffi = FFI()

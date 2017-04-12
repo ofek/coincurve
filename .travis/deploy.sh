66e6d903c825e583bf92ab2fefde448f43a3db1a
ofek <ofekmeister@gmail.com>
2017-04-12 02:42:57 -0400

    typo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,7 @@ python setup.py sdist
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
     docker run --rm -e BUILD_GMP_CPU="amd64" -v $(pwd):/io quay.io/pypa/manylinux1_x86_64 /io/.travis/build-linux-wheels.sh
     linux32 docker run --rm -e BUILD_GMP_CPU="i386" -v $(pwd):/io quay.io/pypa/manylinux1_i686 /io/.travis/build-linux-wheels.sh
-        .travis/build_windows_wheels.sh
+    .travis/build_windows_wheels.sh
 else
 	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 		python -m pip install wheel

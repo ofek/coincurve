664cc595c299a5550fc179e6b79d99483572b082
ofek <ofekmeister@gmail.com>
2017-04-18 21:44:36 -0400

    only upload wheels

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -37,6 +37,6 @@ python -m pip install twine
 # Ignore non-existing files in globs
 shopt -s nullglob
 
-twine upload --skip-existing dist/coincurve*.{whl,gz} -u "${PYPI_USERNAME}"
+twine upload --skip-existing dist/coincurve*.whl -u "${PYPI_USERNAME}"
 
 set +e +x

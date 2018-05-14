6001e46fab92d93912d9d69112776e58005be4e9
Ofek Lev <ofekmeister@gmail.com>
2018-05-14 00:52:38 -0400

    .

--- .travis/install.sh
+++ .travis/install.sh
@@ -73,7 +73,7 @@ if [[ $TRAVIS_OS_NAME == "osx" ]]; then
 		wget https://github.com/certifi/python-certifi/archive/2018.04.16.tar.gz
 		sudo cp 2018.04.16.tar.gz "/Applications/Python ${TRAVIS_PYTHON_VERSION}/2018.04.16.tar.gz"
 		sudo cp install_certificates.command "/Applications/Python ${TRAVIS_PYTHON_VERSION}/Install Certificates.command"
-		chmod +x "/Applications/Python ${TRAVIS_PYTHON_VERSION}/Install Certificates.command"
+		sudo chmod +x "/Applications/Python ${TRAVIS_PYTHON_VERSION}/Install Certificates.command"
 		sudo ./install_certificates.command
 	fi
 

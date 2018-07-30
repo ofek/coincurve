60bc86362003d15dbc7cd1dabd02133d18de2c49
Ofek Lev <ofekmeister@gmail.com>
2018-07-30 00:52:12 -0400

    try build

--- setup.py
+++ setup.py
@@ -262,7 +262,7 @@ else:
 
 setup(
     name='coincurve',
-    version='8.0.1',
+    version='8.0.2-rc.1',
 
     description='Cross-platform Python CFFI bindings for libsecp256k1',
     long_description=open('README.rst', 'r').read(),

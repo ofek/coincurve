a58d5cdccf2b8ad28f904246cd4369ba04b5d432
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-22 14:51:14 -0700

    Using `delocate` for wheels built on OS X.
    Fixes #19.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,8 +16,23 @@ if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
     .travis/build_windows_wheels.sh
 else
 	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-		python -m pip install wheel
-		python setup.py bdist_wheel
+		# Make sure we can build and "fix" the wheel.
+		python -m pip install delocate wheel
+		# Create directories for the built and fixed wheels.
+		mkdir dist_wheels/ fixed_wheels/
+		# Build the wheel for the local OS.
+		python -m pip wheel . --wheel-dir dist_wheels/
+		# Make the wheel relocatable to another OS.
+		delocate-wheel \
+			--check-archs \
+			--wheel-dir fixed_wheels/ \
+			--verbose \
+			dist_wheels/coincurve*.whl
+		# Move the fixed wheel into dist/.
+		[ -d dist/ ] || mkdir dist/
+		mv fixed_wheels/coincurve*.whl dist/
+		# Clean up build directories.
+		rm -fr dist_wheels/ fixed_wheels/
 	fi
 
 	if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then

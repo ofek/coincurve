971d07fe79ec36b5b60ae103e40ce32f8f61efc4
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-22 15:28:56 -0700

    Using `delocate` for wheels built on OS X. (#24)
    * Using `delocate` for wheels built on OS X.
    
    Fixes #19.
    
    * Replacing all tabs with 4 spaces in `deploy.sh`.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -15,20 +15,35 @@ if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
     linux32 docker run --rm -e BUILD_GMP_CPU="i686" -v $(pwd):/io quay.io/pypa/manylinux1_i686 /io/.travis/build-linux-wheels.sh
     .travis/build_windows_wheels.sh
 else
-	if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-		python -m pip install wheel
-		python setup.py bdist_wheel
-	fi
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        # Make sure we can build and "fix" the wheel.
+        python -m pip install delocate wheel
+        # Create directories for the built and fixed wheels.
+        mkdir dist_wheels/ fixed_wheels/
+        # Build the wheel for the local OS.
+        python -m pip wheel . --wheel-dir dist_wheels/
+        # Make the wheel relocatable to another OS.
+        delocate-wheel \
+            --check-archs \
+            --wheel-dir fixed_wheels/ \
+            --verbose \
+            dist_wheels/coincurve*.whl
+        # Move the fixed wheel into dist/.
+        [ -d dist/ ] || mkdir dist/
+        mv fixed_wheels/coincurve*.whl dist/
+        # Clean up build directories.
+        rm -fr dist_wheels/ fixed_wheels/
+    fi
 
-	if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then
-		python -m pip install wheel
-		python setup.py bdist_wheel
-		python3 -m pip install wheel auditwheel pyelftools typing
-		wget -q https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 && tar -xjpf patchelf-*.tar.bz2 && cd patchelf* && ./configure > /dev/null && sudo make install > /dev/null && cd ..
-		auditwheel repair dist/coincurve*.whl
-		rm dist/coincurve*.whl
-		mv wheelhouse/coincurve*.whl dist
-	fi
+    if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then
+        python -m pip install wheel
+        python setup.py bdist_wheel
+        python3 -m pip install wheel auditwheel pyelftools typing
+        wget -q https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 && tar -xjpf patchelf-*.tar.bz2 && cd patchelf* && ./configure > /dev/null && sudo make install > /dev/null && cd ..
+        auditwheel repair dist/coincurve*.whl
+        rm dist/coincurve*.whl
+        mv wheelhouse/coincurve*.whl dist
+    fi
 fi
 
 ls -l dist

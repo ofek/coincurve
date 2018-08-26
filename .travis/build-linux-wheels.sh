174cb45508b8ae6c8167d95fc4479beaf1852e0c
Ofek Lev <ofekmeister@gmail.com>
2018-08-26 19:59:19 -0400

    6

--- .travis/build-linux-wheels.sh
+++ .travis/build-linux-wheels.sh
@@ -9,15 +9,27 @@ yum install -y pkg-config libffi libffi-devel
 # Use updated GMP
 curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2 && tar -xjpf gmp-*.tar.bz2 && cd gmp* && ./configure --build=${BUILD_GMP_CPU}-pc-linux-gnu > /dev/null && make > /dev/null && make check > /dev/null && make install > /dev/null && cd ..
 
+mkdir out
+
+if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then
+    curl -O https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-6.0.0-linux_x86_64-portable.tar.bz2
+    tar -jxvf pypy3.5-6.0.0-linux_x86_64-portable.tar.bz2
+    pypy3.5-6.0.0-linux_x86_64-portable/bin/python -m pip install wheel
+    pypy3.5-6.0.0-linux_x86_64-portable/bin/python -m pip wheel /io/ -w wheelhouse/
+    pypy3.5-6.0.0-linux_x86_64-portable/bin/python -m pip install wheel pyelftools typing
+    pypy3.5-6.0.0-linux_x86_64-portable/bin/python -m pip install -e git://github.com/pypa/auditwheel.git@fb6f76d4262dbb76a6ea068000e71fdfe6fd06ee#egg=auditwheel
+    curl -O https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 && tar -xjpf patchelf-*.tar.bz2 && cd patchelf* && ./configure > /dev/null && sudo make install > /dev/null && cd ..
+    pypy3.5-6.0.0-linux_x86_64-portable/bin/python -m auditwheel repair wheelhouse/coincurve*.whl -w out
+fi
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
 	if [[ ${PYBIN} =~ (cp27|cp35|cp36|cp37) ]]; then
-	${PYBIN}/pip wheel /io/ -w wheelhouse/
+	    ${PYBIN}/pip wheel /io/ -w wheelhouse/
     fi
 done
 
 # Adjust wheel tags
-mkdir out
 for whl in wheelhouse/coincurve*.whl; do
     auditwheel repair $whl -w out
 done
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,7 +11,7 @@ mkdir dist
 python setup.py sdist
 
 if [[ "$TRAVIS_OS_NAME" == "linux" && ${BUILD_LINUX_WHEELS} -eq 1 ]]; then
-    docker run --rm -e BUILD_GMP_CPU="amd64" -v $(pwd):/io quay.io/pypa/manylinux1_x86_64 /io/.travis/build-linux-wheels.sh
+    docker run --rm -e BUILD_GMP_CPU="amd64" -e TRAVIS_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION -v $(pwd):/io quay.io/pypa/manylinux1_x86_64 /io/.travis/build-linux-wheels.sh
     linux32 docker run --rm -e BUILD_GMP_CPU="i686" -v $(pwd):/io quay.io/pypa/manylinux1_i686 /io/.travis/build-linux-wheels.sh
     .travis/build_windows_wheels.sh
 else
@@ -33,17 +33,6 @@ else
         mv fixed_wheels/coincurve*.whl dist/
         # Clean up build directories.
         rm -fr dist_wheels/ fixed_wheels/
-    fi
-
-    if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then
-        python -m pip install wheel
-        python setup.py bdist_wheel
-        python3 -m pip install wheel pyelftools typing
-        python3 -m pip install -e git://github.com/pypa/auditwheel.git@fb6f76d4262dbb76a6ea068000e71fdfe6fd06ee#egg=auditwheel
-        wget -q https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 && tar -xjpf patchelf-*.tar.bz2 && cd patchelf* && ./configure > /dev/null && sudo make install > /dev/null && cd ..
-        auditwheel repair dist/coincurve*.whl
-        rm dist/coincurve*.whl
-        mv wheelhouse/coincurve*.whl dist
     fi
 fi
 

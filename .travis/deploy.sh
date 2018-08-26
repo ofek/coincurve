faa3f36551fd1316ac4a495f41cf3e70a4296e7d
Ofek Lev <ofekmeister@gmail.com>
2018-08-26 18:40:39 -0400

    5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -38,7 +38,8 @@ else
     if [[ "$TRAVIS_PYTHON_VERSION" == "pypy3" ]]; then
         python -m pip install wheel
         python setup.py bdist_wheel
-        python3 -m pip install wheel auditwheel==1.9.0 pyelftools typing
+        python3 -m pip install wheel pyelftools typing
+        python3 -m pip install -e git://github.com/pypa/auditwheel.git@fb6f76d4262dbb76a6ea068000e71fdfe6fd06ee#egg=auditwheel
         wget -q https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2 && tar -xjpf patchelf-*.tar.bz2 && cd patchelf* && ./configure > /dev/null && sudo make install > /dev/null && cd ..
         auditwheel repair dist/coincurve*.whl
         rm dist/coincurve*.whl

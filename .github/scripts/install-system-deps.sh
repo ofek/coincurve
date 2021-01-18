3f07a68255d1402b27beab0f5a12cfa7734304c4
Ofek Lev <ofekmeister@gmail.com>
2021-01-17 22:58:58 -0500

    refactor for github actions (#77)
    * refactor for github actions
    
    * .

--- .github/scripts/install-system-deps.sh
+++ .github/scripts/install-system-deps.sh
@@ -6,13 +6,13 @@ set -x
 if [[ "$OS_NAME" =~ "macos-" ]]; then
 
     # update brew
-    brew update || brew update
+    brew update
 
     # Update openssl if necessary
     brew outdated openssl || brew upgrade openssl
 
     # Install packages needed to build lib-secp256k1
-    for pkg in automake libtool pkg-config libffi; do
+    for pkg in automake libtool pkg-config; do
         brew list $pkg > /dev/null || brew install $pkg
         brew outdated --quiet $pkg || brew upgrade $pkg
     done

d5181635df0f84a849e850e45d2366a530e7a4ec
Ofek Lev <ofekmeister@gmail.com>
2021-01-18 15:41:34 -0500

    build wheels with manylinux2014 (#80)
    * build wheels with manylinux2014
    
    * .
    
    * .

--- .github/scripts/build-linux-wheels.sh
+++ .github/scripts/build-linux-wheels.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Install system packages required by our library
-yum install -y pkg-config libffi libffi-devel
+yum install -y pkgconfig libffi libffi-devel
 
 # Use updated GMP
 curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2 && tar -xjpf gmp-*.tar.bz2 && cd gmp* && ./configure --build=${BUILD_GMP_CPU}-pc-linux-gnu > /dev/null && make > /dev/null && make check > /dev/null && make install > /dev/null && cd ..

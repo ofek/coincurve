7e604baf280de2b81acba6051b47b8a05f7a6cbf
Ofek Lev <ofekmeister@gmail.com>
2021-01-18 19:32:36 -0500

    upgrade libgmp

--- .github/scripts/build-linux-wheels.sh
+++ .github/scripts/build-linux-wheels.sh
@@ -7,7 +7,7 @@ set -x
 yum install -y pkgconfig libffi libffi-devel
 
 # Use updated GMP
-curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.bz2 && tar -xjpf gmp-*.tar.bz2 && cd gmp* && ./configure --build=${BUILD_GMP_CPU}-pc-linux-gnu > /dev/null && make > /dev/null && make check > /dev/null && make install > /dev/null && cd ..
+curl -O https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.bz2 && tar -xjpf gmp-*.tar.bz2 && cd gmp* && ./configure --build=${BUILD_GMP_CPU}-pc-linux-gnu > /dev/null && make > /dev/null && make check > /dev/null && make install > /dev/null && cd ..
 
 mkdir out
 

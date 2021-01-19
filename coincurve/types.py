7d7a2d0a82a0e4c8e8f34ca091fe97173daff6dc
Ofek Lev <ofekmeister@gmail.com>
2021-01-19 09:46:26 -0500

    Update types.py

--- coincurve/types.py
+++ coincurve/types.py
@@ -1,4 +1,10 @@
-from typing import Callable, Optional
+import sys
+from typing import Optional
 
 # https://bugs.python.org/issue42965
+if sys.version_info >= (3, 9, 2):
+    from collections.abc import Callable
+else:
+    from typing import Callable
+
 Hasher = Optional[Callable[[bytes], bytes]]

1c2fb198edd6af70fa21458c78e7e063c523fdf3
Ofek Lev <ofekmeister@gmail.com>
2021-12-04 21:36:01 -0500

    .

--- .github/scripts/build-windows-wheels.sh
+++ .github/scripts/build-windows-wheels.sh
@@ -9,6 +9,14 @@ build_dll() {
     make
 }
 
+mkdir .hidden
+cp * .hidden -R
+mkdir .hidden/dist
+mv .hidden/coincurve/_windows_libsecp256k1.py .hidden/coincurve/_libsecp256k1.py
+mv .hidden ../clean
+sudo apt-get install -y mingw-w64
+sudo apt-get -f install
+
 cd ..
 
 curl -sLO "https://github.com/bitcoin-core/secp256k1/archive/$COINCURVE_UPSTREAM_REF.tar.gz"

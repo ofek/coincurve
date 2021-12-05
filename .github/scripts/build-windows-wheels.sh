f1ed1464a07c5818ecabeca7bd2c254d2f751c57
Ofek Lev <ofekmeister@gmail.com>
2021-12-04 22:10:21 -0500

    .

--- .github/scripts/build-windows-wheels.sh
+++ .github/scripts/build-windows-wheels.sh
@@ -9,13 +9,13 @@ build_dll() {
     make
 }
 
-mkdir .hidden
-cp * .hidden -R
-mkdir .hidden/dist
-mv .hidden/coincurve/_windows_libsecp256k1.py .hidden/coincurve/_libsecp256k1.py
-mv .hidden ../clean
 sudo apt-get install -y mingw-w64
 sudo apt-get -f install
+
+mkdir .hidden
+cp * .hidden -R
+mv .hidden/coincurve/_windows_libsecp256k1.py .hidden/coincurve/_libsecp256k1.py
+mv .hidden ../clean
 
 cd ..
 

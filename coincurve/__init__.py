f13612f377bfc857922364ae397789ff1e98c46d
Weiliang Li <to.be.impressive@gmail.com>
2022-08-04 22:06:37 +0900

    Add __all__ (#113)
    * Add __all__
    
    Closes #112
    
    * Update __init__.py

--- coincurve/__init__.py
+++ coincurve/__init__.py
@@ -1,3 +1,11 @@
 from coincurve.context import GLOBAL_CONTEXT, Context
 from coincurve.keys import PrivateKey, PublicKey
 from coincurve.utils import verify_signature
+
+__all__ = [
+    'GLOBAL_CONTEXT',
+    'Context',
+    'PrivateKey',
+    'PublicKey',
+    'verify_signature',
+]

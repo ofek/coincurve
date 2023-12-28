d315b130d103b3a66d986cfe252247312b00ac78
memento <claude.rc@gmail.com>
2023-12-27 18:03:21 -0600

    (fix) Do we need env?

--- setup_support.py
+++ setup_support.py
@@ -62,9 +62,9 @@ def _find_lib():
     from cffi import FFI
 
     try:
-        subprocess.check_output(['pkg-config', '--exists', 'libsecp256k1'], env=env)  # noqa S603
+        subprocess.check_output(['pkg-config', '--exists', 'libsecp256k1'])  # noqa S603
 
-        includes = subprocess.check_output(['pkg-config', '--cflags-only-I', 'libsecp256k1'], env=env)  # noqa S603
+        includes = subprocess.check_output(['pkg-config', '--cflags-only-I', 'libsecp256k1'])  # noqa S603
         includes = includes.strip().decode('utf-8')
 
         return os.path.exists(os.path.join(includes[2:], 'secp256k1_ecdh.h'))
@@ -75,7 +75,7 @@ def _find_lib():
                 with suppress(OSError):
                     FFI().dlopen(path)
                     return True
-        # We couldn't locate libsecp256k1 so we'll use the bundled one
+        # We couldn't locate libsecp256k1, so we'll use the bundled one
         return False
 
 

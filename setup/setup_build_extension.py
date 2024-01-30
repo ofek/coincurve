b23d6dcd5fc5407d1b9c1e835049774f6fe48389
memento <claude.rc@gmail.com>
2024-01-30 16:03:25 -0600

    (dbg) isn't that weird?

--- setup/setup_build_extension.py
+++ setup/setup_build_extension.py
@@ -52,8 +52,8 @@ class BuildCFFISetuptools(_build_ext):
                 #     os.rename(os.path.join(lib_dir, _a), os.path.join(lib_dir, lib_file))
                 # self.spwan(['lib', '/DEF:' + lib_fp, '/OUT:' + lib_file.replace('.a', '.lib')])
                 # self.spawn(['nm', '-g', f'{lib_dir}/{lib_file}'])  # using a msys2 command with unix path
-                args = ['nm', '-g', f'{lib_dir}/{lib_file}']
-                # log.info(subprocess.check_output(args, shell=True, check=True))  # noqa S603
+                # args = ['nm', '-g', f'{lib_dir}/{lib_file}']
+                # log.info(subprocess.check_output(args, shell=True, check=True))  # S603
                 # self.spawn(['dumpbin.exe', '/ALL', f'{lib_dir}/{lib_file}'.replace('/', '\\\\')])
                 link_args_msvc.append(lib_file)
             else:

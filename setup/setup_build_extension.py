5c16d5da5ae3079d90192b440c1c938987fc28dc
memento <claude.rc@gmail.com>
2024-01-30 10:06:04 -0600

    (dbg) typo

--- setup/setup_build_extension.py
+++ setup/setup_build_extension.py
@@ -51,7 +51,7 @@ class BuildCFFISetuptools(_build_ext):
                 #     # lib_file = lib_file.replace('.a', '.lib')
                 #     os.rename(os.path.join(lib_dir, _a), os.path.join(lib_dir, lib_file))
                 # self.spwan(['lib', '/DEF:' + lib_fp, '/OUT:' + lib_file.replace('.a', '.lib')])
-                self.spwan(['ar', '-t', os.path.join(lib_dir, lib_file)])
+                self.spawn(['ar', '-t', str(os.path.join(lib_dir, lib_file))])
                 link_args_msvc.append(lib_file.replace('.a', '.lib'))
             else:
                 self.extensions[0].extra_link_args.append(lib_fp)

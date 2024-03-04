d34cde2bd868bcd4d8b74aba2d64da5794be2fe1
memento <claude.rc@gmail.com>
2024-03-04 14:59:25 -0600

    (fix) dllimport

--- _cffi_build/build_shared.py
+++ _cffi_build/build_shared.py
@@ -31,7 +31,13 @@ def gather_sources_from_directory(directory: str) -> List[Source]:
 define_static_lib = """
 #if defined(_WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #   define SECP256K1_STATIC 1
-/* #   define SECP256K1_API extern __declspec(dllexport) */
+#endif
+"""
+
+
+define_shared_lib = """
+#if defined(_WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#   define SECP256K1_API extern __declspec(dllimport)
 #endif
 """
 
@@ -48,8 +54,9 @@ def mk_ffi(sources: List[Source],
     :return: An FFI object.
     """
     _ffi = FFI()
-    code = [define_static_lib] if static_lib else []
+    code = [define_static_lib] if static_lib else [define_shared_lib]
 
+    logging.info(f'   Static {static_lib}...')
     for source in sources:
         with open(os.path.join(here, source.h)) as h:
             logging.info(f'   Including {source.h}...')

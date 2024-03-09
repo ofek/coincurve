7b87106fb268255142c4989d1d81f555a27bd256
memento <claude.rc@gmail.com>
2024-03-09 10:47:57 -0600

    (fix) add library loader at package initilization

--- coincurve/__init__.py
+++ coincurve/__init__.py
@@ -4,24 +4,12 @@ import platform
 import warnings
 
 from coincurve._secp256k1_library_info import SECP256K1_LIBRARY_NAME, SECP256K1_LIBRARY_TYPE
-from coincurve.context import GLOBAL_CONTEXT, Context
-from coincurve.keys import PrivateKey, PublicKey, PublicKeyXOnly
-from coincurve.utils import verify_signature
-
-__all__ = [
-    'GLOBAL_CONTEXT',
-    'SECP256K1_LIBRARY_TYPE',
-    'SECP256K1_LIBRARY_NAME',
-    'Context',
-    'PrivateKey',
-    'PublicKey',
-    'PublicKeyXOnly',
-    'verify_signature',
-]
 
 
 def load_secp256k1_conda_library():
+    """Load the secp256k1 library from the conda environment."""
     if SECP256K1_LIBRARY_TYPE != 'EXTERNAL':
+        warnings.warn(f'DBG: {SECP256K1_LIBRARY_NAME}:{SECP256K1_LIBRARY_TYPE}', stacklevel=2)
         return
 
     if (conda := os.getenv('CONDA_PREFIX')) is None:
@@ -39,3 +27,21 @@ def load_secp256k1_conda_library():
         ctypes.CDLL(library)
     except Exception as e:
         warnings.warn(f'The required library {SECP256K1_LIBRARY_NAME}.so/dylib/dll is not loaded.\n{e}', stacklevel=2)
+
+
+load_secp256k1_conda_library()
+
+from coincurve.context import GLOBAL_CONTEXT, Context  # noqa: E402
+from coincurve.keys import PrivateKey, PublicKey, PublicKeyXOnly  # noqa: E402
+from coincurve.utils import verify_signature  # noqa: E402
+
+__all__ = [
+    'GLOBAL_CONTEXT',
+    'SECP256K1_LIBRARY_TYPE',
+    'SECP256K1_LIBRARY_NAME',
+    'Context',
+    'PrivateKey',
+    'PublicKey',
+    'PublicKeyXOnly',
+    'verify_signature',
+]

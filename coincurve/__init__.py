7bdcba826f03488ccc71612d33b515943f6d0dcb
memento <claude.rc@gmail.com>
2024-03-09 13:10:00 -0600

    (ref) lazy imports

--- coincurve/__init__.py
+++ coincurve/__init__.py
@@ -1,20 +1,20 @@
-import ctypes
-import os
-import platform
-import warnings
-
-from coincurve._secp256k1_library_info import SECP256K1_LIBRARY_NAME, SECP256K1_LIBRARY_TYPE
-
-
 def load_secp256k1_conda_library():
     """Load the secp256k1 library from the conda environment."""
+    import warnings
+
+    from coincurve._secp256k1_library_info import SECP256K1_LIBRARY_NAME, SECP256K1_LIBRARY_TYPE
+
     if SECP256K1_LIBRARY_TYPE != 'EXTERNAL':
         warnings.warn(f'DBG: {SECP256K1_LIBRARY_NAME}:{SECP256K1_LIBRARY_TYPE}', stacklevel=2)
         return
 
+    import os
+
     if (conda := os.getenv('CONDA_PREFIX')) is None:
         warnings.warn('This coincurve package requires a CONDA environment', stacklevel=2)
         return
+
+    import platform
 
     if platform.system() == 'Windows':
         library = os.path.join(conda, 'Library', 'bin', f'{SECP256K1_LIBRARY_NAME}.dll')
@@ -24,6 +24,8 @@ def load_secp256k1_conda_library():
         library = os.path.join(conda, 'lib', f'{SECP256K1_LIBRARY_NAME}.so')
 
     try:
+        import ctypes
+
         ctypes.CDLL(library)
     except Exception as e:
         warnings.warn(f'The required library {SECP256K1_LIBRARY_NAME}.so/dylib/dll is not loaded.\n{e}', stacklevel=2)
@@ -37,8 +39,6 @@ from coincurve.utils import verify_signature  # noqa: E402
 
 __all__ = [
     'GLOBAL_CONTEXT',
-    'SECP256K1_LIBRARY_TYPE',
-    'SECP256K1_LIBRARY_NAME',
     'Context',
     'PrivateKey',
     'PublicKey',

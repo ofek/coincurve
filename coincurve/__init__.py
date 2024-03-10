fd83fe46b24bb8e8b3b076dd38ad7e19abe4896b
memento <claude.rc@gmail.com>
2024-03-10 14:17:22 -0500

    (fix) import snafu

--- coincurve/__init__.py
+++ coincurve/__init__.py
@@ -1,5 +1,7 @@
 def load_secp256k1_conda_library():
     """Load the secp256k1 library from the conda environment."""
+    import warnings
+
     from coincurve._secp256k1_library_info import SECP256K1_LIBRARY_NAME, SECP256K1_LIBRARY_TYPE
 
     if SECP256K1_LIBRARY_TYPE != 'EXTERNAL':
@@ -7,13 +9,13 @@ def load_secp256k1_conda_library():
         return
 
     import os
+    from ctypes import CDLL
+    from ctypes.util import find_library
 
     try:
-        import ctypes.util import find_library
-
         # Find the library in the typical installation paths
-        if lib := find_library(SECP256K1_LIBRARY_NAME):
-            ctypes.CDLL(lib)
+        if library := find_library(SECP256K1_LIBRARY_NAME):
+            CDLL(library)
             return
 
         # Find the library in the conda environment
@@ -25,9 +27,9 @@ def load_secp256k1_conda_library():
             else:
                 library = os.path.join(conda, 'lib', SECP256K1_LIBRARY_NAME)
 
-            ctypes.CDLL(library)
+            CDLL(library)
+            return
     except Exception as e:
-        import warnings
         warnings.warn(f'The required library {SECP256K1_LIBRARY_NAME}l is not loaded.\n{e}', stacklevel=2)
 
 

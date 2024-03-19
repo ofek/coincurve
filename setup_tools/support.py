c1917e1f7d75d48e2d252876a14a4a7894ed5920
memento <claude.rc@gmail.com>
2024-03-19 11:11:57 -0500

    (fix) update_pkgconfig fails when path is None

--- setup_tools/support.py
+++ setup_tools/support.py
@@ -128,7 +128,7 @@ def define_secp256k1_local_lib_info():
     return LIB_NAME, 'x_lib'
 
 
-def update_pkg_config_path(path='.'):
+def update_pkg_config_path(path=None):
     """Updates the PKG_CONFIG_PATH environment variable to include the given path."""
     pkg_config_paths = [path, os.getenv('PKG_CONFIG_PATH', '').strip('"')]
 
@@ -140,7 +140,7 @@ def update_pkg_config_path(path='.'):
         pkg_config_paths.append(os.path.join(lbd, 'pkgconfig'))
 
     # Update environment
-    os.environ['PKG_CONFIG_PATH'] = os.pathsep.join(pkg_config_paths)
+    os.environ['PKG_CONFIG_PATH'] = os.pathsep.join([p for p in pkg_config_paths if p is not None])
 
 
 def verify_system_lib(lib_dir):

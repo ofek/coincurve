246293b356fc2f64d648504527905a50688b5db2
memento <claude.rc@gmail.com>
2024-04-21 16:37:07 -0500

    (ref) update version for hatch

--- src/coincurve/__about__.py
+++ src/coincurve/__about__.py
@@ -1,9 +1,10 @@
-MAJOR = 19
-MINOR = 0
-MICRO = 1
-RELEASE = True
-
-__version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-if not RELEASE:
-    __version__ += '.dev0'
+# MAJOR = 19
+# MINOR = 0
+# MICRO = 1
+# RELEASE = True
+#
+# __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+#
+# if not RELEASE:
+#     __version__ += '.dev0'
+__version__ = '19.0.1.dev'

1c6258207ff8637df8a907d4476fbef9969c6fd1
Ofek Lev <ofekmeister@gmail.com>
2024-06-01 14:51:44 -0400

    try

--- src/coincurve/__about__.py
+++ src/coincurve/__about__.py
@@ -1,10 +1,1 @@
-# MAJOR = 19
-# MINOR = 0
-# MICRO = 1
-# RELEASE = True
-#
-# __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-#
-# if not RELEASE:
-#     __version__ += '.dev0'
 __version__ = '19.0.2'

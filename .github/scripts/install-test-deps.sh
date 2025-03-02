f0a68dc6441976c3fe5308eb927de7497a826e05
Ofek Lev <ofekmeister@gmail.com>
2025-03-02 15:39:41 -0500

    Various updates (#176)

--- .github/scripts/install-test-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-if [ "$RUNNER_OS" == "macOS" ]; then
-    ./.github/scripts/install-macos-build-deps.sh
-fi
-
-python -m pip install --upgrade cffi
-python -m pip install --upgrade tox codecov

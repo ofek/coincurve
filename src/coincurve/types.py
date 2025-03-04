6bf37214e06b6a8cef41c0da9922a0b8070e9f72
Ofek Lev <ofekmeister@gmail.com>
2025-03-04 17:34:26 -0500

    Update docs (#180)

--- src/coincurve/types.py
+++ src/coincurve/types.py
@@ -1,14 +1,8 @@
 from __future__ import annotations
 
-import sys
+from collections.abc import Callable
 
 from coincurve._libsecp256k1 import ffi
-
-# https://bugs.python.org/issue42965
-if sys.version_info >= (3, 9, 2):
-    from collections.abc import Callable
-else:
-    from typing import Callable
 
 Hasher = Callable[[bytes], bytes] | None
 Nonce = tuple[ffi.CData, ffi.CData]

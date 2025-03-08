25cba0154688f03ddc194b63cc1348b7e3a1e0b0
Ofek Lev <ofekmeister@gmail.com>
2025-03-07 22:46:03 -0500

    prepare (#183)
    * prepare
    
    * Update benchmarks.md

--- src/coincurve/__init__.py
+++ src/coincurve/__init__.py
@@ -2,6 +2,7 @@ from coincurve.context import GLOBAL_CONTEXT, Context
 from coincurve.keys import PrivateKey, PublicKey, PublicKeyXOnly
 from coincurve.utils import verify_signature
 
+__version__ = "20.0.0"
 __all__ = [
     "GLOBAL_CONTEXT",
     "Context",
